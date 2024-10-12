e4d7f0c605377de5e3552db58a9fe08948065c5e
DanielaNahir <danielanahir927@gmail.com>
2024-10-12 22:38:31 +0000

    ADD: scripts y punto a

---
+++ RTA_SCRIPT_Examen_20241012/Punto_A.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# punto A
+
+# el directorio se crea en la raiz (/)
+
+# creo el directorio
+
+	mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+	echo "directorio creado"
+	
